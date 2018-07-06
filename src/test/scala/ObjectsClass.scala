13de20f6188d0bcf46d664977c6ac78e02dc0f1e
Jeniffer Florez Londoño <jeniflorezl95@gmail.com>
2018-07-06 16:30:18 -0500

    add others tests

--- src/test/scala/ObjectsClass.scala
+++ src/test/scala/ObjectsClass.scala
@@ -22,6 +22,8 @@ class ObjectsClass extends FunSuite{
     Movie.academyAwardBestMoviesForYear(1780) getOrElse("No found") should be("No found")
   }
 
+  //Companion object
+
   class Person(val name: String, private val superHeroName: String)
 
   object Person{
@@ -34,6 +36,58 @@ class ObjectsClass extends FunSuite{
 
     Person.showMeTheSecret(clark) should be("Superman")
     Person.showMeTheSecret(peter) should be("Spider-Man")
+  }
+
+  test("companion objects"){
+    class Person(val name: String, val age: Int)
+
+    object Person{
+      def newPerson(str: String) = {
+        str.split("\\ ") match {
+          case Array(str1, str2) => Some(new Person(str1, str2.toInt))
+          case _ => None
+        }
+      }
+    }
+
+    val person1 = Person.newPerson("Angela 22")
+    person1.get.name should be("Angela")
+    person1.get.age should be(22)
+
+    val date = raw"(\d{4})-(\d{2})-(\d{2})".r
+    val regex = "2004-01-20" match {
+      case date(_*) => "It's a date!"
+    }
+    println(regex)
+
+    val embeddedDate = date.unanchored
+    "Date: 2004-01-20 17:25:18 GMT (10 years, 28 weeks, 5 days, 17 hours and 51 minutes ago)" match {
+      case embeddedDate("2004", "01", "20") => println("A Scala is born.")
+    }
+
+  }
+
+  test("extractor objects"){
+    object CustommingMessage{
+      def apply(name: String) = "Hola " + name + " !!"
+
+      def unapply(custommingMessage: String): Option[String] = {
+        val array = custommingMessage.split("\\ ")
+        if (array.tail.nonEmpty) Some(array(1)) else None
+      }
+    }
+
+    val applyCustom = CustommingMessage("jeni")
+    applyCustom should be("Hola jeni !!")
+    val result = applyCustom match {
+      case CustommingMessage(name) => println(name)
+      case _ => println("error")
+    }
+    result should be("jeni")
+
+    //asigning variable
+    val otherWay = CustommingMessage("jeni")
+    val CustommingMessage(name) = otherWay
   }
 
 }
--- src/test/scala/PatternMatchingTest.scala
+++ src/test/scala/PatternMatchingTest.scala
@@ -53,5 +53,84 @@ class PatternMatchingTest extends FunSuite{
     r == Nil should be(true)
   }
 
+  test("case class with pattern matching"){
+    abstract class Person
+
+    case class Employee(name: String, salario: Double) extends Person
+
+    case class Client(name: String, company: String) extends Person
+
+    case class Provider(name: String, product: String) extends Person
+
+    def chooseTypePerson(person: Person): String = person match {
+      case Employee(name: String, salario: Double) =>
+        "El empleado " + name + " gana " + salario.toInt + " pesos"
+      case Client(name: String, company: String) =>
+        "El cliente " + name + " viene de la empresa " + company
+      case Provider(name: String, product: String) =>
+        "El proveedor " + name + " nos distribuye " + product
+    }
+
+    val employee = Employee("Camilo", 1000000)
+    val client = Client("Gonzalo", "Sura")
+    val provider = Provider("Carlos", "Chocolates")
+    chooseTypePerson(employee) should be("El empleado Camilo gana 1000000 pesos")
+    chooseTypePerson(client) should be("El cliente Gonzalo viene de la empresa Sura")
+    chooseTypePerson(provider) should be("El proveedor Carlos nos distribuye Chocolates")
+  }
+
+  test("case class with pattern matching Pattern Guards (condicionals)"){
+    abstract class Person
+
+    case class Employee(name: String, salario: Double) extends Person
+
+    case class Client(name: String, company: String) extends Person
+
+    case class Provider(name: String, product: String) extends Person
+
+    def chooseTypePersonCondicional(person: Person, importPerson: Seq[String]): String = person match {
+      case Employee(name: String, salario: Double) if importPerson.contains("employee") =>
+        "El empleado " + name + " gana " + salario.toInt + " pesos"
+      case Client(name: String, company: String) if importPerson.contains("client") =>
+        "El cliente " + name + " viene de la empresa " + company
+      case Provider(name: String, product: String) if importPerson.contains("provider") =>
+        "El proveedor " + name + " nos distribuye " + product
+      case other => "nothing"
+    }
+
+    val importPerson = Seq("client", "provider")
+    val employee = Employee("Camilo", 1000000)
+    val client = Client("Gonzalo", "Sura")
+    val provider = Provider("Carlos", "Chocolates")
+    chooseTypePersonCondicional(employee, importPerson) should be("nothing")
+    chooseTypePersonCondicional(client, importPerson) should be("El cliente Gonzalo viene de la empresa Sura")
+    chooseTypePersonCondicional(provider, importPerson) should be("El proveedor Carlos nos distribuye Chocolates")
+  }
+
+  test("matching on the type only"){
+    abstract class Transport
+
+    case class Car(time: String, cost: Double) extends Transport {
+      def info = "Se demora " + time + " y cuesta " + cost.toInt
+    }
+
+    case class Subway(time: String, cost: Double) extends Transport {
+      def info = "Se demora " + time + " y cuesta " + cost.toInt
+    }
+
+    case class Bus(time: String, cost: Double) extends Transport {
+      def info = "Se demora " + time + " y cuesta " + cost.toInt
+    }
+
+    def conveyance(transport: Transport) = transport match {
+      case c: Car => c.info
+      case s: Subway => s.info
+      case b: Bus => b.info
+    }
+
+    val car = conveyance(Car("1 hr", 23000)) should be("Se demora 1 hr y cuesta 23000")
+
+  }
+
 
 }
