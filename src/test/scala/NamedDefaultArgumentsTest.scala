edaf8e6ec62741f0b829c7e9256ea06ca84a8c9d
Jeniffer Florez Londoño <jeniflorezl95@gmail.com>
2018-07-10 17:01:52 -0500

    add tests

---
+++ src/test/scala/NamedDefaultArgumentsTest.scala
@@ -0,0 +1,25 @@
+import org.scalatest.FunSuite
+import org.scalatest.Matchers._
+
+class NamedDefaultArgumentsTest extends FunSuite{
+
+  test("use name of variables explicitly"){
+    def printName(first: String = "Jhon", last: String = "Suarez"): String ={
+      val name = first + ", " + last
+      name
+    }
+
+    printName("Angel", "Gonzales") should be("Angel, Gonzales")
+    printName(first = "Esteban", "Cardona") should be("Esteban, Cardona")
+    printName(last = "Lopez", first = "Daniel") should be("Daniel, Lopez")
+
+    printName(last = "Londoño") should be("Jhon, Londoño")
+  }
+
+  test("default parameters can be functional too"){
+    def reduce(a: Int, f: (Int, Int) => Int = _ + _): Int = f(a, a)
+    reduce(5) should be(10)
+    reduce(5, _ * _) should be(25)
+  }
+
+}
